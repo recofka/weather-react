f5ca04304b453d34f49dedf0cfc4a697d341edeb
Denise Recofka <recofka@gmail.com>
2020-05-05 21:14:35 +0200

    Update constants protocol  (#15)
    * Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const api = {
     key: "28dbe2b27fc7519a174dbfb1e49d7533",
-    baseURL: "http://api.openweathermap.org/data/2.5/"
+    baseURL: "https://api.openweathermap.org/data/2.5/"
   }
   
