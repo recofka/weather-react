3aade8122cb6968ddfd71decfb32cc834c7d0dc6
Denise Recofka <recofka@gmail.com>
2020-05-07 22:46:16 +0200

    Adding eslint, start with styling and Material Ui (#16)

--- src/components/Location/Location.js
+++ src/components/Location/Location.js
@@ -1,13 +1,28 @@
+/* eslint-disable react/display-name */
+/* eslint-disable react/prop-types */
 import React from "react";
+import RoomIcon from '@material-ui/icons/Room';
 
 export default ({ city, location, date }) => {
     if (!city) {
-        return (<div> </div>)
+        return (
+            <div className="location-box">
+                <div className="location">
+                    <p className="location-city">Type your city</p>
+                    {/* <RoomIcon className="location-icon"/> */}
+                </div>
+            </div>
+        )
     }
     return (
         <div className="location-box">
-            <div className="location">{city} - {location}</div>
-            <div className="date">{date}</div>
+            <div className="location">
+                <p className="location-city">{city} - {location}</p>
+                <RoomIcon className="location-icon" />
+            </div>
+            <div className="location-date">
+                <p className="date">{date}</p>
+            </div>
         </div>)
 };
 
--- src/components/Location/LocationContainer.js
+++ src/components/Location/LocationContainer.js
@@ -2,18 +2,9 @@ import React from 'react';
 import Location from './Location';
 import { Unix_timestamp } from '../../Helper';
 import { connect } from 'react-redux';
+import './index.scss';
 
 class LocationContainer extends React.Component {
-    dataBuilder = (d) => {
-        let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-        let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-        let day = days[d.getDay()];
-        let date = d.getDate();
-        let month = months[d.getMonth()];
-        let year = d.getFullYear();
-        return `${day} ${date} ${month} ${year}`
-    }
-
     render() {
         const { country, city, date } = this.props;
         return (
