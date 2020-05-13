1222e108d6e9c780bd5ec7ce0ba2f67d9b24adee
Denise Recofka <recofka@gmail.com>
2020-05-13 19:28:13 +0200

    Improve code (#27)
    * Adjusting code based in tips from a experienced FE developer

--- src/components/Location/Location.js
+++ src/components/Location/Location.js
@@ -4,25 +4,17 @@ import React from "react";
 import { Unix_timestamp } from '../../Helper';
 import RoomIcon from '@material-ui/icons/Room';
 
-export default ({ city, location, date }) => {
-    if (!city) {
-        return (
-            <div className="location-box">
-                <div className="location">
-                    <p className="location-city">Type your city</p>
-                </div>
-            </div>
-        )
-    }
-    return (
-        <div className="location-box">
-            <div className="location">
-                <p className="location-city">{city} - {location}</p>
-                <RoomIcon className="location-icon" />
-            </div>
+export default ({ city, country, date }) => (
+    <div className="location-box">
+        <div className="location">
+            <p className="location-city">
+                {city ? `${city} - ${country}` : 'Type your city'}
+            </p>
+            {city && <RoomIcon className="location-icon" />}
+        </div>
+        {city &&
             <div className="location-date">
                 <p className="date">{Unix_timestamp(date)}</p>
-            </div>
-        </div>)
-};
-
+            </div>}
+    </div>
+);
