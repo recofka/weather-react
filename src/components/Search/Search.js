39b8bde1f22c54871cb9206a72fde5ee3f415007
Denise Recofka <recofka@gmail.com>
2020-05-13 19:29:16 +0200

    Development (#28)
    * Improve code (#25)
    
    * changing mapstate to Optional_chaining, to clean code
    
    * change components file to not use React.Components
    
    * Improve code (#27)
    
    * Adjusting code based in tips from a experienced FE developer

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,13 +1,25 @@
 import React from 'react';
 
-export default ({ getWeather }) => (
-    <div className="search-box">
-        <form onSubmit={getWeather}>
-            <input
-                type="text"
-                name="city"
-                className="search-bar"
-                placeholder="Search..."
-            />
-        </form>
-    </div>)
+const Search = ({ fetchCoordinates }) => {
+    const getWeather = (e) => {
+        e.preventDefault();
+        return (
+          fetchCoordinates(e.target.city.value),
+          e.target.city.value = '')
+    }
+
+    return (
+        <div className="search-box">
+            <form onSubmit={getWeather}>
+                <input
+                    type="text"
+                    name="city"
+                    className="search-bar"
+                    placeholder="Search..."
+                />
+            </form>
+        </div>
+    )
+};
+
+export default Search; 
